ebcd55e2b61aef190abc1b78fc2ca3e4046a15a0
Themrpie <73369706+Themrpie@users.noreply.github.com>
2022-05-01 16:23:21 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import FinancialItem from "./components/FinancialItem";
+
+import {Provider} from 'react-redux'
+import store from "./store";
+
+function App(){
+  return (
+      <Provider store={store}>
+          <div className="App">
+              <FinancialItem/>
+          </div>
+      </Provider>
+  );
+}
+
+export default App;
