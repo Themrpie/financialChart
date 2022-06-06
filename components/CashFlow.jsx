a6ab494cd86373f3c2198a7d3bef89cbeb9d3061
Themrpie <73369706+Themrpie@users.noreply.github.com>
2022-06-06 19:37:06 -0400

    Add files via upload
    Optimized code to plot DataTable component used in CashFlow and IncomeStatement

--- components/CashFlow.jsx
+++ components/CashFlow.jsx
@@ -14,9 +14,14 @@ import PropTypes from 'prop-types'
 import {getFinancialItem} from "../actions/financialItem";
 import {getCashFlow} from "../actions/cashFlow";
 
-//TODO:
-//ADD symbol to IncomeStatement
-//ADD getIncomeStatement(symbol, timeFrame)
+//Creates name
+function unCamel (str){
+    return str
+        // insert a space between lower & upper
+         .replace(/([A-Z])/g, ' $1')
+        // uppercase the first character
+        .replace(/^./, function(str){ return str.toUpperCase(); })
+}
 
 const CashFlow = ({cashFlow:{cashFlow},getCashFlow, statement, symbol}) => {
     const classes = financialItemStyle();
@@ -33,67 +38,8 @@ const CashFlow = ({cashFlow:{cashFlow},getCashFlow, statement, symbol}) => {
     },[]);
 
 
-    const displayTheRightPlot = () => { 
-        switch(statement){
-            case 'operatingCashflow':
-                return (<DataTable financialItemName={'Operating Cash Flow'} financialItem={cashFlow} statement={'operatingCashflow'}/>);
-            case 'paymentsForOperatingActivities':
-                return (<DataTable financialItemName={'Payments For Operating Activities'} financialItem={cashFlow} statement={'paymentsForOperatingActivities'}/>);
-            case 'netIncome':
-                return (<DataTable financialItemName={'Net Income'} financialItem={cashFlow} statement={'netIncome'}/>);
-            case 'proceedsFromOperatingActivities':
-                return (<DataTable financialItemName={'Proceeds From Operating Activities'} financialItem={cashFlow} statement={'proceedsFromOperatingActivities'}/>);
-             case 'changeInOperatingLiabilities':
-                return (<DataTable financialItemName={'Change In Operating Liabilities'} financialItem={cashFlow} statement={'changeInOperatingLiabilities'}/>);
-             case 'changeInOperatingAssets':
-                return (<DataTable financialItemName={'Change in Operating Assets'} financialItem={cashFlow} statement={'changeInOperatingAssets'}/>);
-             case 'paymentsForRepurchaseOfEquityDepletionAndAmortization':
-                return (<DataTable financialItemName={'Payments For Payments For Repurchase Of Equity Depletion & Amortization'} financialItem={cashFlow} statement={'paymentsForRepurchaseOfEquityDepletionAndAmortization'}/>);
-             case 'capitalExpenditures':
-                return (<DataTable financialItemName={'Capital Expenditures'} financialItem={cashFlow} statement={'capitalExpenditures'}/>);
-             case 'changeInReceivables':
-                return (<DataTable financialItemName={'Change In Receivables'} financialItem={cashFlow} statement={'changeInReceivables'}/>);            
-            case 'changeInInventory':
-                return (<DataTable financialItemName={'Change In Inventory'} financialItem={cashFlow} statement={'changeInInventory'}/>);
-            case 'profitLoss':
-                return (<DataTable financialItemName={'Profit / Loss'} financialItem={cashFlow} statement={'profitLoss'}/>);
-            case 'cashflowFromInvestment':
-                return (<DataTable financialItemName={'Cashflow From Investment'} financialItem={cashFlow} statement={'cashflowFromInvestment'}/>);
-            case 'cashflowFromFinancing':
-                return (<DataTable financialItemName={'Cashflow From Financing'} financialItem={cashFlow} statement={'cashflowFromFinancing'}/>);
-            case 'proceedsFromRepaymentsOfShortTermDebt':
-                return (<DataTable financialItemName={'Proceeds From Repayment Of Short Term Debt'} financialItem={cashFlow} statement={'proceedsFromRepaymentsOfShortTermDebt'}/>);
-            case 'paymentsForRepurchaseOfCommonStock':
-                return (<DataTable financialItemName={'Payments For Repurchase Of Common Stock'} financialItem={cashFlow} statement={'paymentsForRepurchaseOfCommonStock'}/>);
-                case 'paymentsForRepurchaseOfEquity':
-                return (<DataTable financialItemName={'Payments For Repurchase Of Equity'} financialItem={cashFlow} statement={'paymentsForRepurchaseOfEquity'}/>);
-            case 'paymentsForRepurchaseOfPreferredStock':
-                return (<DataTable financialItemName={'Payments For Repurchase Of Preferred Stock'} financialItem={cashFlow} statement={'paymentsForRepurchaseOfPreferredStock'}/>);
-            case 'dividendPayout':
-                return (<DataTable financialItemName={'Dividen Payout'} financialItem={cashFlow} statement={'dividendPayout'}/>);
-            case 'dividendPayoutCommonStock':
-                return (<DataTable financialItemName={'Dividen Payout Common Stock'} financialItem={cashFlow} statement={'dividendPayoutCommonStock'}/>);
-            case 'dividendPayoutPreferredStock':
-                return (<DataTable financialItemName={'Dividend Payout Preferred Stock'} financialItem={cashFlow} statement={'dividendPayoutPreferredStock'}/>);
-            case 'proceedsFromIssuanceOfCommonStock':
-                return (<DataTable financialItemName={'Proceeds From Issuance Of Common Stock'} financialItem={cashFlow} statement={'proceedsFromIssuanceOfCommonStock'}/>);
-            case 'proceedsFromIssuanceOfPreferredStock':
-                return (<DataTable financialItemName={'Proceeds From Issuance Of Preferred Stock'} financialItem={cashFlow} statement={'proceedsFromIssuanceOfPreferredStock'}/>);
-            case 'proceedsFromRepurchaseOfEquity':
-                return (<DataTable financialItemName={'Proceeds From Repurchase Of Equity'} financialItem={cashFlow} statement={'proceedsFromRepurchaseOfEquity'}/>);
-            case 'proceedsFromSaleOfTreasuryStock':
-                return (<DataTable financialItemName={'Proceeds From Sale Of Trasury Stock'} financialItem={cashFlow} statement={'proceedsFromSaleOfTreasuryStock'}/>);
-            case 'changeInCashAndCashEquivalents':
-                return (<DataTable financialItemName={'Change In Cash And Cash Equivalents'} financialItem={cashFlow} statement={'changeInCashAndCashEquivalents'}/>);
-            case 'changeInExchangeRate':
-                return (<DataTable financialItemName={'Change In Exchange Rate'} financialItem={cashFlow} statement={'changeInExchangeRate'}/>);
-            case 'proceedsFromIssuanceOfLongTermDebtAndCapitalSecuritiesNet':
-                return (<DataTable financialItemName={'Proceeds From Issuance Of Long Term Debt & Capital Securities Net'} financialItem={cashFlow} statement={'changeInExchangeRate'}/>);
-           
-        } 
-           
-                
-            
+    const displayTheRightPlot = () => {            
+        return (<DataTable financialItemName={unCamel(statement)} financialItem={cashFlow} statement={statement}/>);             
     };
 // For some reason cashFlow is not defined
     return (
